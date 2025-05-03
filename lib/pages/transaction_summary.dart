9ba535bb925a92ba4573020813e751008a3fc975
Widiya Marin <widiyamarin@gmail.com>
2025-05-03 14:31:53 +0800

    update  menampilkan total keseluruhan  transaksi

---
+++ lib/pages/transaction_summary.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class TransactionSummary extends StatelessWidget {
+  final List<Map<String, dynamic>> summarydata;
+
+  const TransactionSummary({super.key, required this.summarydata});
+
+  @override
+  Widget build(BuildContext context) {
+    final totalIncome = summarydata
+        .where((tx) => tx['isExpense'] == false)
+        .fold(0.0, (sum, tx) => sum + tx['amount']);
+
+    final totalExpense = summarydata
+        .where((tx) => tx['isExpense'] == true)
+        .fold(0.0, (sum, tx) => sum + tx['amount']);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 55, 57, 74),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Income Section
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Icon(Icons.account_balance_wallet,
+                      color: Colors.green, size: 15),
+                ),
+                const SizedBox(width: 12),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Income',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 194, 166, 249),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 4),
+                    Text('Rp. ${totalIncome.toStringAsFixed(0)}',
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ],
+            ),
+
+            // Outcome Section
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Icon(Icons.account_balance_wallet,
+                      color: Colors.red, size: 15),
+                ),
+                const SizedBox(width: 12),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Outcome',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 194, 166, 249),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 4),
+                    Text('Rp. ${totalExpense.toStringAsFixed(0)}',
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
