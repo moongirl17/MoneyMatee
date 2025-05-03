255c7f94751ad7c7f1412d01dd23072dfb14bd42
Widiya Marin <widiyamarin@gmail.com>
2025-05-03 20:02:51 +0800

    update  mode light  blm fix

--- lib/pages/transaction_form.dart
+++ lib/pages/transaction_form.dart
@@ -2,53 +2,49 @@ import 'package:flutter/material.dart';
 
 class TransactionForm extends StatefulWidget {
   const TransactionForm({
-    super.key, 
-    this.category, 
-    this.amount, 
+    super.key,
+    this.category,
+    this.amount,
     this.date,
-    this.isExpense, // Tambahkan parameter isExpense
+    this.isExpense,
   });
 
-  final String? category; // Kategori transaksi
-  final String? amount; // Jumlah transaksi
-  final DateTime? date; // Tanggal transaksi
-  final bool? isExpense; // Status expense, nullable
-  
+  final String? category;
+  final String? amount;
+  final DateTime? date;
+  final bool? isExpense;
+
   @override
   State<TransactionForm> createState() => _TransactionFormState();
 }
 
 class _TransactionFormState extends State<TransactionForm> {
-  bool isExpense = true; // Default value for the switch
-  final TextEditingController _categoryController = TextEditingController(); // Controller untuk kategori
-  final TextEditingController _dateController = TextEditingController(); // Controller untuk tanggal
-  final TextEditingController _amountController = TextEditingController(); // Controller untuk jumlah
-  final _formKey = GlobalKey<FormState>(); // Kunci untuk form
-  DateTime? _selectedDate; // Variabel untuk menyimpan tanggal yang dipilih
+  bool _isExpense = true; // Hindari konflik nama
+  final TextEditingController _categoryController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  DateTime? _selectedDate;
 
-  @override
-  void dispose() {
-    _categoryController.dispose(); // Dispose controller saat widget dihapus
-    _dateController.dispose(); // Dispose controller saat widget dihapus
-    _amountController.dispose(); // Dispose controller saat widget dihapus
-    super.dispose();
-  }
-  
   @override
   void initState() {
     super.initState();
     _categoryController.text = widget.category ?? '';
-    _amountController.text = widget.amount?.toString() ?? '';
-    
-    // Set default untuk isExpense
-    isExpense = widget.isExpense ?? true; // Gunakan nilai dari widget atau default ke true
-    
-    // Hapus pengaturan default untuk _dateController
+    _amountController.text = widget.amount ?? '';
+    _isExpense = widget.isExpense ?? true;
+
     if (widget.date != null) {
-      _dateController.text =
-          '${widget.date!.day}/${widget.date!.month}/${widget.date!.year}';
+      _selectedDate = widget.date;
+      _dateController.text = '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}';
     }
-    _selectedDate = widget.date; // Set tanggal yang dipilih dari widget
+  }
+
+  @override
+  void dispose() {
+    _categoryController.dispose();
+    _amountController.dispose();
+    _dateController.dispose();
+    super.dispose();
   }
 
   @override
@@ -62,95 +58,98 @@ class _TransactionFormState extends State<TransactionForm> {
       ),
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Form( // Bungkus dengan Form
-            key: _formKey, // Tetapkan _formKey di sini
+          child: Form(
+            key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(height: 20), // Jarak vertikal di bawah AppBar
+                const SizedBox(height: 20),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
                   child: Row(
                     children: [
                       Switch(
-                        value: isExpense,
+                        value: _isExpense,
                         onChanged: (bool value) {
                           setState(() {
-                            isExpense = value;
+                            _isExpense = value;
                           });
                         },
                         inactiveTrackColor: Colors.green,
-                        inactiveThumbColor: Colors.green[200],  
+                        inactiveThumbColor: Colors.green[200],
                         activeColor: Colors.red[200],
                         activeTrackColor: Colors.red,
-
                       ),
                       Text(
-                        isExpense ? 'Expense' : 'Income',
+                        _isExpense ? 'Expense' : 'Income',
                         style: const TextStyle(color: Colors.white, fontSize: 14),
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 30), // Jarak antara switch dan elemen berikutnya
+                const SizedBox(height: 30),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
                   child: TextFormField(
-                    controller: _amountController, // Controller untuk jumlah
+                    controller: _amountController,
                     keyboardType: TextInputType.number,
-                    style: const TextStyle(color: Colors.white), // Warna teks input
+                    style: const TextStyle(color: Colors.white),
                     decoration: const InputDecoration(
                       labelText: 'Amount',
-                      labelStyle: TextStyle(color: Colors.white), // Warna label
+                      labelStyle: TextStyle(color: Colors.white),
                       enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)), // Border radius saat tidak fokus
-                        borderSide: BorderSide(color: Colors.white), // Warna border saat tidak fokus
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide(color: Colors.white),
                       ),
                       focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)), // Border radius saat fokus
-                        borderSide: BorderSide(color: Colors.white), // Warna border saat fokus
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide(color: Colors.white),
                       ),
                     ),
                     validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter Amount';
-                    }
-                    return null;
+                      if (value == null || value.trim().isEmpty) {
+                        return 'Please enter Amount';
+                      }
+                      final amount = double.tryParse(value.trim());
+                      if (amount == null || amount <= 0) {
+                        return 'Enter a valid positive amount';
+                      }
+                      return null;
                     },
                   ),
                 ),
-                const SizedBox(height: 20), // Jarak antara Amount dan Kategori
+                const SizedBox(height: 20),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
                   child: TextFormField(
-                    controller: _categoryController, // Controller untuk kategori
-                    style: const TextStyle(color: Colors.white), // Warna teks input
+                    controller: _categoryController,
+                    style: const TextStyle(color: Colors.white),
                     decoration: const InputDecoration(
                       labelText: 'Category',
-                      labelStyle: TextStyle(color: Colors.white), // Warna label
+                      labelStyle: TextStyle(color: Colors.white),
                       enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)), // Border radius saat tidak fokus
-                        borderSide: BorderSide(color: Colors.white), // Warna border saat tidak fokus
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide(color: Colors.white),
                       ),
                       focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)), // Border radius saat fokus
-                        borderSide: BorderSide(color: Colors.white), // Warna border saat fokus
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide(color: Colors.white),
                       ),
                     ),
                     validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter Category';
-                    }
-                    return null;
-                  },
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter Category';
+                      }
+                      return null;
+                    },
                   ),
                 ),
-                const SizedBox(height: 20), // Jarak antara Kategori dan DatePicker
+                const SizedBox(height: 20),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
                   child: TextFormField(
-                    controller: _dateController, // Controller untuk tanggal
-                    readOnly: true, // Membuat field hanya bisa dibaca
+                    controller: _dateController,
+                    readOnly: true,
                     onTap: () async {
                       DateTime? pickedDate = await showDatePicker(
                         context: context,
@@ -158,68 +157,67 @@ class _TransactionFormState extends State<TransactionForm> {
                         firstDate: DateTime(2000),
                         lastDate: DateTime(2101),
                       );
-                      if (pickedDate != null && pickedDate != _selectedDate) {
+                      if (pickedDate != null) {
                         setState(() {
                           _selectedDate = pickedDate;
                           _dateController.text =
-                              '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'; // Format tanggal
+                              '${pickedDate.day}/${pickedDate.month}/${pickedDate.year}';
                         });
                       }
                     },
-                    style: const TextStyle(color: Colors.white), // Warna teks input
-                    decoration: InputDecoration(
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
                       labelText: 'Enter Date',
-                      labelStyle: const TextStyle(color: Colors.white), // Warna label
-                      enabledBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)), // Border radius saat tidak fokus
-                        borderSide: BorderSide(color: Colors.white), // Warna border saat tidak fokus
+                      labelStyle: TextStyle(color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide(color: Colors.white),
                       ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)), // Border radius saat fokus
-                        borderSide: BorderSide(color: Colors.white), // Warna border saat fokus
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide(color: Colors.white),
                       ),
-                      suffixIcon: const Icon(
+                      suffixIcon: Icon(
                         Icons.calendar_today,
                         color: Colors.white,
-                        size: 18, // Ukuran ikon lebih kecil
+                        size: 18,
                       ),
                     ),
                     validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please select a Date';
-                    }
-                    return null;
-                  },
+                      if (value == null || value.isEmpty) {
+                        return 'Please select a Date';
+                      }
+                      return null;
+                    },
                   ),
                 ),
-                const SizedBox(height: 50), // Tambahkan jarak antara DatePicker dan tombol Save
+                const SizedBox(height: 50),
                 Center(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16), // Tambahkan padding di sekitar tombol
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
                     child: SizedBox(
-                      width: 200, // Atur lebar tombol agar tidak terlalu lebar
+                      width: 200,
                       child: ElevatedButton(
                         onPressed: () {
                           if (_formKey.currentState?.validate() ?? false) {
-                            final category = _categoryController.text;
-                            final amount = double.tryParse(_amountController.text) ?? 0.00;
-                            final date = _selectedDate ?? DateTime.now(); // Gunakan tanggal yang dipilih atau tanggal saat ini
+                            final category = _categoryController.text.trim();
+                            final amount = double.tryParse(_amountController.text.trim()) ?? 0.00;
+                            final date = _selectedDate ?? DateTime.now();
 
-                            // Kembalikan data ke halaman sebelumnya
                             Navigator.pop(context, {
                               'category': category,
                               'amount': amount,
                               'date': date,
-                              'isExpense': isExpense, // Pastikan isExpense selalu dikembalikan
+                              'isExpense': _isExpense,
                             });
                           }
                         },
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color.fromARGB(255, 194, 166, 249), // Warna tombol
+                          backgroundColor: const Color.fromARGB(255, 194, 166, 249),
                           shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20), // Border radius tombol
+                            borderRadius: BorderRadius.circular(20),
                           ),
-                          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15), // Padding dalam tombol
+                          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                         ),
                         child: const Text(
                           'Save',
