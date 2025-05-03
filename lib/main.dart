7ba749f4e8f9e518114c307dceacd11cafb9ef83
Widiya Marin <widiyamarin@gmail.com>
2025-05-03 20:05:37 +0800

    update theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:moneymate/theme/theme_cubit.dart';
+import 'package:moneymate/theme/theme_localstorage.dart';
+import 'package:moneymate/injector.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get_it/get_it.dart';
 import 'package:moneymate/pages/home.dart';
-void main() {
+// import 'package:flutter_app/Tugas_1/register.dart'; // Import halaman register
+// import 'package:flutter_app/Tugas_1/login.dart'; // Import halaman login
+// import 'package:flutter_app/day-3/home.dart';
+
+// Removed unused import
+// Removed unused import
+final getIt = GetIt.instance;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupInjector();
   runApp(const MyApp());
 }
 
@@ -8,24 +23,38 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MoneyMate',
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 37, 39, 52),
-        appBarTheme: AppBarTheme(
-          // backgroundColor: Color.fromARGB(255, 194, 166, 249),
-           backgroundColor: Color.fromARGB(255, 55, 57, 74),
-          foregroundColor:  Color.fromARGB(255, 217, 203, 244),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(30),
-              bottomRight: Radius.circular(30),
+   Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeCubit>(
+          create: (context) => ThemeCubit(
+            ThemeLocalStorage(
+              getIt<SharedPreferences>(),
             ),
-          ),
+          )..init(),
         ),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, state) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            themeMode: state,
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
+            // routes: routes,
+            // initialRoute: AppRoutes.home,
+           home: const Home(),
+            
+          );
+        }
       ),
-      home: const Home(),
+      // initialRoute: '/login', // Halaman awal adalah login
+      // routes: {
+      //   '/login': (context) => const Login(title: 'Login'), // Halaman login
+      //   '/register': (context) => const Register(title: 'Register'), // Halaman register
+      //   '/home': (context) => const Homepage(title: 'Home'), // Halaman home
+      // },
     );
   }
 }
+
